e32697103ee6072e60a7bfdb43b251e7fd36bb92
Xiraj <akbarsirajuddinh@gmail.com>
2023-06-19 22:49:36 +0700

    update blog#2

--- src/components/blogItem.tsx
+++ src/components/blogItem.tsx
@@ -7,7 +7,7 @@ const blogItem = () => {
     <div className='flex flex-row'>
         <Image alt="icon Blog" src="/images/blogIcon.png" width={200} height={200} className='rounded-lg w-[80px] h-[80px]'/>
         <div className='ml-[32px] flex flex-col justify-between'>
-            <Link href='https://github.com/Xiraj/My-Portofolio' className='text-lg font-bold hover:text-zinc-600'>Blog Title</Link>
+            <Link href='https://github.com/Xiraj/My-Portofolio' className='text-lg font-bold hover:text-zinc-600'>My Portofolio</Link>
             <div className='inline-flex gap-[32px] text-sm'>
                 <span>2 hari yang lalu</span>
                 <span>181 dilihat</span>
